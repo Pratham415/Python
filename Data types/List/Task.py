10c9b19dcbc8118407afb63ec06749244f504f13
Pratham Patel <pratham0401patel@gmail.com>
2025-03-07 19:08:43 +0530

    list

--- Data types/List/Task.py
+++ Data types/List/Task.py
@@ -1,49 +1,72 @@
 veggies = [
-    "Bitter Gourd (Karela)",
-    "Bottle Gourd (Lauki)",
-    "Ridge Gourd (Turai)",
-    "Snake Gourd (Galka)",
-    "Pointed Gourd (Parwal)",
-    "Drumstick (Moringa)",
-    "Taro Root (Arbi)",
-    "Elephant Foot Yam (Suran)",
-    "Ash Gourd (Petha)",
-    "Ivy Gourd (Tindora)",
-    "Fenugreek Leaves (Methi)",
-    "Amaranth Leaves (Patra)",
-    "Colocasia Leaves (Arbi ke Patte)",
-    "Beet Greens (Chukandar ke Patte)",
-    "Mustard Greens (Sarson)",
-    "Radish Greens (Mooli ke Patte)",
-    "Spinach (Palak)",
-    "Fenugreek Seeds Sprouts (Methi Dana)",
-    "Cluster Beans (Gawar Phali)",
-    "French Beans",
-    "Yardlong Beans (Bora)",
-    "Broad Beans (Sem)",
-    "Green Peas (Matar)",
-    "Corn (Makai)",
-    "Bamboo Shoots (Karil)",
-    "Jackfruit (Kathal)",
-    "Raw Banana (Kachcha Kela)",
-    "Green Papaya (Kachcha Papita)",
-    "Indian Gooseberry (Amla)",
-    "Curry Leaves (Kadi Patta)",
-    "Raw Mango (Keri)",
-    "Sponge Gourd (Nenua)",
-    "Brinjal (Baingan)",
-    "Lady Finger (Bhindi)",
-    "Cabbage (Patta Gobi)",
-    "Cauliflower (Phool Gobi)",
-    "Radish (Mooli)",
-    "Turnip (Shalgam)",
-    "Carrot (Gajar)",
-    "Tomato (Tamatar)"
-]
+        "Bitter Gourd (Karela)",
+        "Bottle Gourd (Lauki)",
+        "Ridge Gourd (Turai)",
+        "Snake Gourd (Galka)",
+        "Pointed Gourd (Parwal)",
+        "Drumstick (Moringa)",
+        "Taro Root (Arbi)",
+        "Elephant Foot Yam (Suran)",
+        "Ash Gourd (Petha)",
+        "Ivy Gourd (Tindora)",
+        "Fenugreek Leaves (Methi)",
+        "Amaranth Leaves (Patra)",
+        "Colocasia Leaves (Arbi ke Patte)",
+        "Beet Greens (Chukandar ke Patte)",
+        "Mustard Greens (Sarson)",
+        "Radish Greens (Mooli ke Patte)",
+        "Spinach (Palak)",
+        "Fenugreek Seeds Sprouts (Methi Dana)",
+        "Cluster Beans (Gawar Phali)",
+        "French Beans",
+        "Yardlong Beans (Bora)",
+        "Broad Beans (Sem)",
+        "Green Peas (Matar)",
+        "Corn (Makai)",
+        "Bamboo Shoots (Karil)",
+        "Jackfruit (Kathal)",
+        "Raw Banana (Kachcha Kela)",
+        "Green Papaya (Kachcha Papita)",
+        "Indian Gooseberry (Amla)",
+        "Curry Leaves (Kadi Patta)",
+        "Raw Mango (Keri)",
+        "Sponge Gourd (Nenua)",
+        "Brinjal (Baingan)",
+        "Lady Finger (Bhindi)",
+        "Cabbage (Patta Gobi)",
+        "Cauliflower (Phool Gobi)",
+        "Radish (Mooli)",
+        "Turnip (Shalgam)",
+        "Carrot (Gajar)",
+        "Tomato (Tamatar)"
+    ]
 # 1. Sort by Hindi Names
 #  Sort the veggies list based on the Hindi names while keeping the original structure intact.
 # for i in veggies:
 #     print(i[i.find('('):i.find(')') + 1])
 
 # 2. Extract Only Hindi Names
-#  Create a new list that contains only the Hindi names of the vegetables
+#  Create a new list that contains only the Hindi names of the vegetables.
+# lis = []
+# for i in veggies:
+#     word = i[i.find('('):i.find(')') + 1]
+#     lis.append(word)    
+# print(lis)
+
+# 3. Extract Only English Names
+#  Create a new list that contains only the English names of the vegetables.
+eng_word = []
+for i in veggies:
+    word = i[0:i.find(" (")]
+    eng_word.append(word)
+print(eng_word)
+
+# 4. Find the Longest and Shortest Vegetable Names
+#  Identify and print the longest and shortest vegetable names (English and Hindi separately).
+# English :
+max = 0
+for i in eng_word:
+    if len(i) > max:
+        max = len(i)
+        print(i)
+print(max)
---
+++ Data types/List/Task1.py
@@ -0,0 +1,13 @@
+# pdf 2 
+# 1. Rotate a list by n places:
+# Rotate a list by n places:
+# Input: 
+# lst = [1, 2, 3, 4, 5] , n = 2
+# Output: 
+# [4, 5, 1, 2, 3]
+num = int(input("Enter the number : "))
+lst = [1, 2, 3, 4, 5]
+print(lst[2:5] + lst[0:2])
+for i in lst:
+    print(i)
+
